3ce446c07251502174c9d14e8f850e714a84d9ba
Eliboll <eetes77@gmail.com>
2025-05-07 14:08:19 -0500

    added transaction class again

---
+++ Transactions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction_Tracker
+{
+    class Transaction
+    {
+        public DateTime date;
+        public string account;
+        public string payee;
+        public string description;
+        public string category;
+        public float amount;
+
+        public Transaction(DateTime date, string account, string payee, string description, string category, float amount) 
+        {
+            this.date           = date;
+            this.account        = account;
+            this.payee          = payee;
+            this.description    = description;
+            this.category       = category;
+            this.amount         = amount;
+        }
+    }
+}
