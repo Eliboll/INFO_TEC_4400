492570e0146dac987707968e185dfd58a8c33f10
Ryan Milewski <36171248+rmilewski4@users.noreply.github.com>
2025-05-08 16:21:07 -0500

    Add exit button functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -263,5 +263,9 @@ namespace Transaction_Tracker
             }
         }
 
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
